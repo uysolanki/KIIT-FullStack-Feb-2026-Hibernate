89157fe57e6a58a3812784d63d1b60f91f5bda46
uysolanki <uysolanki@gmail.com>
2026-03-23 19:57:06 +0530

    save

--- src/main/java/com/kiit/lms/App.java
+++ src/main/java/com/kiit/lms/App.java
@@ -1,7 +1,11 @@
 package com.kiit.lms;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import com.kiit.lms.model.Student;
 
 public class App 
 {
@@ -13,6 +17,27 @@ public class App
     	SessionFactory factory=cfg.buildSessionFactory();
 
     	System.out.println(factory.isClosed());  //false
+    	
+    	Session session1=factory.openSession();
+    	Transaction tx=session1.beginTransaction();
+    	Student s1=new Student();
+    	s1.setSname("Alice");
+    	s1.setPer(98.5);
+    	
+    	Student s2=new Student();
+    	s2.setSname("Ben");
+    	s2.setPer(88.5);
+    	
+    	Student s3=new Student();
+    	s3.setSname("Chris");
+    	s3.setPer(78.5);
+    	
+    	session1.save(s1);	//insert into..
+    	session1.save(s2);	
+    	session1.save(s3);	
+    	tx.commit();
+    	session1.close();
+    	factory.close();
 
     }
 }
---
+++ src/main/java/com/kiit/lms/model/Student.java
@@ -0,0 +1,55 @@
+package com.kiit.lms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int rno;
+	private String sname;
+	private double per;
+	public Student(int rno, String sname, double per) {
+		this.rno = rno;
+		this.sname = sname;
+		this.per = per;
+	}
+	
+	public Student() {}
+
+	public int getRno() {
+		return rno;
+	}
+
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public double getPer() {
+		return per;
+	}
+
+	public void setPer(double per) {
+		this.per = per;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rno=" + rno + ", sname=" + sname + ", per=" + per + "]";
+	}
+	
+	
+
+}
